cbb490e38a8252978e4174bb6cbc0e1f051720df
Nero Zato <happyfisherod@gmail.com>
2021-11-14 19:04:16 +0900

    first commit

---
+++ sagas/index.js
@@ -0,0 +1,10 @@
+import { all, call } from 'redux-saga/effects';
+import CurrencyConverterSaga from './/currencyconverter';
+
+function* watchAll() {
+  yield all([
+    call(CurrencyConverterSaga)
+  ]);
+}
+
+export default watchAll;
