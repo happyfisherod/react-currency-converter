8ec9dee8dd63c634ef62178a487cff49bb637699
Nero Zato <happyfisherod@gmail.com>
2021-11-15 17:16:18 +0900

    init commit

--- src/setupTests.js
+++
@@ -1,5 +0,0 @@
-// https://enzymejs.github.io/enzyme/docs/installation/index.html
-import { configure } from "enzyme";
-import Adapter from "enzyme-adapter-react-16";
-
-configure({ adapter: new Adapter() });
